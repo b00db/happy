89eca45ae559db382b74a834e4bca6b5a12c844e
b00db <48580026+b00db@users.noreply.github.com>
2021-12-28 14:56:49 +0900

    Update Posts.java

---
+++ src/main/java/com/welfare/happy/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.welfare.happy.springboot.web.dto;
+
+import com.welfare.happy.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
