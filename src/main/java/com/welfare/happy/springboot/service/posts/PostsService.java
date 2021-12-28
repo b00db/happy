1bf8ea8b111beafcb37659b92367c56fb0d3bbb4
b00db <48580026+b00db@users.noreply.github.com>
2021-12-28 14:12:24 +0900

    Create Posts.java

---
+++ src/main/java/com/welfare/happy/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.welfare.happy.springboot.service.posts;
+
+import com.welfare.happy.springboot.domain.posts.PostsRepository;
+import com.welfare.happy.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
