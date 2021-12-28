32e418125f78c3b63bf7533198decfae371887e3
b00db <48580026+b00db@users.noreply.github.com>
2021-12-28 15:12:38 +0900

    Using JPA Auditing

--- src/main/java/com/welfare/happy/springboot/domain/posts/Posts.java
+++ src/main/java/com/welfare/happy/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.welfare.happy.springboot.domain.posts;
 
+import com.welfare.happy.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
